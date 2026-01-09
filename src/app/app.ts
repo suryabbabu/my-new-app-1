466b6777ef6059265b4398b8930d8f0966cfa187
suryabbabu <suryajr190@gmail.com>
2026-01-09 20:41:33 +0530

    Update app title and test expectation
    Changed the app title signal from 'my-asdads-app' to 'my-surya-app' in App component. Updated the test to expect the new title in the h1 element.

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-asdads-app');
+  protected readonly title = signal('my-surya-app');
 }
